49015945865446400c8631f3c52374b2b0a0c56d
Phathu <you@example.com>
2020-12-22 04:41:53 +0200

    New Work

---
+++ module4-solution/js/speakGoodBye.js
@@ -0,0 +1,9 @@
+
+(function (window) {
+	var byeSpeaker ={};
+	var speakWord = "Good Bye";
+	byeSpeaker.speak = function(name) {
+  	console.log(speakWord + " " + byeSpeaker.name);
+	}
+	window.byeSpeaker = byeSpeaker;
+})(window);
